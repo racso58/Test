016ed9d99d9f04739f4fd5ea9adbdf23d45e3c67
Ruiz Campos Oscar Eloy <oscarruizc58@gmail.com>
2022-03-04 10:37:40 -0600

    Resolucion de AreSame

--- src/main/java/com/solucionfactible/dev/AreSame.java
+++ src/main/java/com/solucionfactible/dev/AreSame.java
@@ -1,5 +1,9 @@
 package com.solucionfactible.dev;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * comp checks whether the two arrays have the "same" elements, with the same multiplicities. "Same" means, 
  * here, that the elements in b are the elements in a squared, regardless of the order.
@@ -9,7 +13,22 @@ package com.solucionfactible.dev;
 public class AreSame {
 	
 	public static boolean comp(int[] a, int[] b) {
-		return false;
+
+		List<Integer> listA = Arrays.stream(a)
+				.boxed()
+				.collect(Collectors.toList());
+
+		List<Integer> listB = Arrays.stream(b)
+				.boxed()
+				.sorted()
+				.collect(Collectors.toList());
+
+		List<Integer> listAP = listA.stream()
+				.map(x -> (int) Math.pow(x, 2))
+				.sorted()
+				.collect(Collectors.toList());
+
+		return listAP.equals(listB);
 	}
 
 }
