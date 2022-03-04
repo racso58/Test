4558a8e250cc2d7fd2daf5c9ea6523941c783916
Oscar Ruiz <oscarruizc58@gmail.com>
2022-03-03 21:51:52 -0600

    ValidatePin Resolve

--- src/main/java/com/solucionfactible/dev/ValidatePIN.java
+++ src/main/java/com/solucionfactible/dev/ValidatePIN.java
@@ -1,5 +1,7 @@
 package com.solucionfactible.dev;
 
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * isValid allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
  * If the function is passed a valid PIN string, return true, else return false.
@@ -9,7 +11,15 @@ package com.solucionfactible.dev;
 public class ValidatePIN {
 	
 	public static boolean isValid(String pin) {
-		return true;
+		if(StringUtils.isNumeric(pin)){
+			if(pin.length() == 4 || pin.length() == 6){
+				return true;
+			}else{
+				return false;
+			}
+		}else{
+			return false;
+		}
 	}
 
 }
