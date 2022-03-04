835e8ff96acffc46b8c1cc3efde4e4dc90be3445
Ruiz Campos Oscar Eloy <oscarruizc58@gmail.com>
2022-03-04 10:19:50 -0600

    Resolucion de ArrayDiff

--- src/main/java/com/solucionfactible/dev/ArrayDiff.java
+++ src/main/java/com/solucionfactible/dev/ArrayDiff.java
@@ -1,5 +1,9 @@
 package com.solucionfactible.dev;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * diff subtracts one list from another and returns the result.
  * It should remove all values from list a, which are present in list b keeping their order.
@@ -9,7 +13,23 @@ package com.solucionfactible.dev;
 public class ArrayDiff {
 	
 	public static int[] diff(int[] a, int[] b) {
-		return a;
+
+		List<Integer> listA = Arrays.stream(a)
+				.boxed()
+				.collect(Collectors.toList());
+		List<Integer> listB = Arrays.stream(b)
+				.boxed()
+				.collect(Collectors.toList());
+
+		List<Integer> finalList = listA.stream()
+				.filter(x -> !listB.contains(x))
+				.collect(Collectors.toList());
+
+		int[] finalArray = finalList.stream()
+				.mapToInt(i -> i)
+				.toArray();
+
+		return finalArray;
 	}
 
 }
