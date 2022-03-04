ec103b99ad3c671aedeb4abe747aa35dd3d89459
Ruiz Campos Oscar Eloy <oscarruizc58@gmail.com>
2022-03-04 08:59:18 -0600

    Se agrega configuracion en pom y resolucion de Pesistence

--- src/main/java/com/solucionfactible/dev/Persistence.java
+++ src/main/java/com/solucionfactible/dev/Persistence.java
@@ -1,5 +1,8 @@
 package com.solucionfactible.dev;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * persistence takes in a positive parameter num and returns its multiplicative persistence, which is the number 
  * of times you must multiply the digits in num until you reach a single digit.
@@ -9,7 +12,29 @@ package com.solucionfactible.dev;
 public class Persistence {
 	
 	public static int persistence(long num) {
-		return 0;
+
+		int count = 0;
+		boolean notExit = true;
+		while (notExit){
+			List<Long> list = new ArrayList<>();
+
+			while(num > 0){
+				list.add(num % 10);
+				num = num / 10;
+			}
+
+			if(list.size() >= 2){
+				num = list.get(0);
+				for (int i = 1; i < list.size(); i++){
+					num *= list.get(i);
+				}
+				count ++;
+			}else {
+				notExit = false;
+			}
+		}
+
+		return count;
 	}
 
 }
